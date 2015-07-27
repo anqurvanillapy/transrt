0c214dc3a06958a55aa90d982931fd7b29b5cf90
AnqurVanillapy <anqurvanillapy@gmail.com>
2015-07-27 10:56:27 +0800

    Updated packing methods.

--- transrt/transrt.py
+++ transrt/transrt.py
@@ -16,6 +16,12 @@ def parse_argument():
         )
 
     parser.add_argument(
+        '-o', '--output',
+        dest='ofile',
+        action='store'
+        )
+
+    parser.add_argument(
         '-n', '--search-number',
         dest='kn',
         action='store'
@@ -51,13 +57,45 @@ class Transrt(object):
             self.srt = open(filename, 'r')
             self.pack(self.srt)
         except IOError:
-            print "Unable to open file %s" % filename
+            raise "Unable to open file '%s'" % filename
 
     def pack(self, srt):
 
-        srt = filter(None, srt.read().split('\n'))
+        print 'Packing file...'
 
-        print srt
+        srt = filter(None, srt.read().split('\n'))
+        srt_pack = []
+        index = 0
+        num = 1
+
+        while index < len(srt):
+            if srt[index].isdigit() and int(srt[index]) == num:
+                    srt_dict = {
+                        'num': None,
+                        'dura': None,
+                        'script': [],
+                        'cont': []
+                    }
+
+                    srt_dict['num'] = num
+                    index += 1
+                    srt_dict['dura'] = srt[index]
+                    index += 1
+
+                    try:
+                        while not (srt[index].isdigit() and int(srt[index]) == num + 1):
+                            srt_dict['cont'].append(srt[index])
+                            index += 1
+                    except:
+                        pass
+
+                    srt_pack.append(srt_dict)
+                    num += 1
+
+            else:
+                index += 1
+
+        print 'Success'
 
     def searchKeynum(self, kn):
 
